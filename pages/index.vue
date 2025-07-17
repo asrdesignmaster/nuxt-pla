f6e9ed225e0725a15ced452e959c458cc841a48e
Altamash Rashid <asr.designmaster@gmail.com>
2025-07-17 19:31:56 +0530

    our work done

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="container">
-    <div class="what_we_do | py-40 text-center">
-      <h2 class="text-4xl font-bold pb-4 text-gray-700">WHAT WE DO</h2>
+    <div class="what_we_do | py-20 pt-40 text-center">
+      <h2 >WHAT WE DO</h2>
       <p class="px-0 lg:px-40 text-xl text-gray-500">Gold Leafing work is an “Art” which now a days became a kind of
         revolution in “Interior Decoration” world. Gold Leafing work is a present and better option against paints. It
         gives royal looks to walls, ceilings, Domes & Furniture etc.</p>
     </div>
-  </div>
-  <div class="container">
-    <div class="leafing | md:py-40 ">
+    <div class="leafing | md:py-20 ">
       <div class="bg-black flex h-[1200px] md:h-[400px] overflow-hidden flex-col md:flex-row">
         <div class="leafing_bg gold_silver_leafing | flex items-center justify-center text-center ">
           <div class="group w-full h-full flex items-center justify-center relative">
@@ -40,7 +38,37 @@
         </div>
       </div>
     </div>
+    <div class="out_work | py-20 text-center">
+      <h2>Our Work</h2>
+      <div class="work_gallery | grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div class="overflow-hidden group">
+          <NuxtImg src="/images/gallery/bunglow-1.webp" alt="Leafing Work 1" class="w-full h-full object-cover transform group-hover:scale-110 transition-transform duration-500" :format="webp" sizes="360px" />
+        </div>
+        <div class="overflow-hidden group">
+          <NuxtImg src="/images/gallery/bunglow-2.webp" alt="Leafing Work 2" class="w-full h-full object-cover transform group-hover:scale-110 transition-transform duration-500" :format="webp" sizes="360px" />
+        </div>
+        <div class="overflow-hidden group">
+          <NuxtImg src="/images/gallery/bunglow-1.webp" alt="Leafing Work 3" class="w-full h-full object-cover transform group-hover:scale-110 transition-transform duration-500" :format="webp" sizes="360px" />
+        </div>
+        <div class="overflow-hidden group">
+          <NuxtImg src="/images/gallery/bunglow-2.webp" alt="Leafing Work 4" class="w-full h-full object-cover transform group-hover:scale-110 transition-transform duration-500" :format="webp" sizes="360px" />
+        </div>
+        <div class="overflow-hidden group">
+          <NuxtImg src="/images/gallery/bunglow-1.webp" alt="Leafing Work 1" class="w-full h-full object-cover transform group-hover:scale-110 transition-transform duration-500" :format="webp" sizes="360px" />
+        </div>
+        <div class="overflow-hidden group">
+          <NuxtImg src="/images/gallery/bunglow-2.webp" alt="Leafing Work 2" class="w-full h-full object-cover transform group-hover:scale-110 transition-transform duration-500" :format="webp" sizes="360px" />
+        </div>
+        <div class="overflow-hidden group">
+          <NuxtImg src="/images/gallery/bunglow-1.webp" alt="Leafing Work 3" class="w-full h-full object-cover transform group-hover:scale-110 transition-transform duration-500" :format="webp" sizes="360px" />
+        </div>
+        <div class="overflow-hidden group">
+          <NuxtImg src="/images/gallery/bunglow-2.webp" alt="Leafing Work 4" class="w-full h-full object-cover transform group-hover:scale-110 transition-transform duration-500" :format="webp" sizes="360px" />
+        </div>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script setup>
