b1c9c349cdff6fae9473c62f2ce0b72d9f35d9ba
Altamash Rashid <asr.designmaster@gmail.com>
2025-07-28 14:43:07 +0530

    Services component created

--- pages/index.vue
+++ pages/index.vue
@@ -89,88 +89,7 @@
       </div>
     </div>
   </div>
-  <div class="out_specialization | container">
-    <div class="py-20 text-center">
-      <h2>Our Specialization</h2>
-      <div class="mt-12  p-0 md:p-8 ">
-        <div class="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-8 text-left">
-          <div class="flex items-start ">
-            <div class="bg-primary p-2 mr-4">
-              <img src="/images/icons/masjid.svg" alt="masjid" class="max-w-[52px]" />
-            </div>
-            <div>
-              <h4 class="font-bold text-gray-800 mb-1">Pure Gold Leafing for Masjid</h4>
-              <p class="text-gray-600">In Masjid gold leafing is done on Mehrab, Mimber, gumbad and ayat.
-                Mostly it's done in original gold but rarely in artificial.</p>
-            </div>
-          </div>
-          <div class="flex items-start">
-            <div class="bg-primary p-2 mr-4">
-              <img src="/images/icons/mandir.svg" alt="mandir" class="max-w-[52px]" />
-            </div>
-            <div>
-              <h4 class="font-bold text-gray-800 mb-1">Shrines & Temples</h4>
-              <p class="text-gray-600">In a residential interior space leafing gives aesthetic looks and it is usually
-                done to enhance the decoration of the space .Gold , Silver, Copper and Champagne leafing is done on
-                ceiling,walls, furniture, centre piece, frames etc.</p>
-            </div>
-          </div>
-          <div class="flex items-start">
-            <div class="bg-primary p-2 mr-4">
-              <img src="/images/icons/flats.svg" alt="flats" class="max-w-[52px]" />
-            </div>
-            <div>
-              <h4 class="font-bold text-gray-800 mb-1">Decoration for Flats</h4>
-              <p class="text-gray-600">In a residential interior space leafing gives aesthetic looks and it is usually
-                done to enhance the decoration of the space. Gold, Silver, Coper & champagne leafing is done on ceiling,
-                walls, furniture, center piece & frames etc.</p>
-            </div>
-          </div>
-          <div class="flex items-start">
-            <div class="bg-primary p-2 mr-4">
-              <img src="/images/icons/leaf.svg" alt="leaf" class="max-w-[52px]" />
-            </div>
-            <div>
-              <h4 class="font-bold text-gray-800 mb-1">Customized Leafing</h4>
-              <p class="text-gray-600">In gold and silver leafing, customization is done as per the client's
-                requirements and according to the theme used in designing .We try our level best to give the outcome as
-                per the client's demand.</p>
-            </div>
-          </div>
-          <div class="flex items-start">
-            <div class="bg-primary p-2 mr-4">
-              <img src="/images/icons/showroom.svg" alt="showroom" class="max-w-[52px]" />
-            </div>
-            <div>
-              <h4 class="font-bold text-gray-800 mb-1">Showrooms</h4>
-              <p class="text-gray-600">In showrooms Gold leafing is done on counter, display, cornice, ceiling, walls,
-                furniture and frames to give a modern and more refined look .</p>
-            </div>
-          </div>
-          <div class="flex items-start">
-            <div class="bg-primary p-2 mr-4">
-              <img src="/images/icons/furniture.svg" alt="furniture" class="max-w-[52px]" />
-            </div>
-            <div>
-              <h4 class="font-bold text-gray-800 mb-1">Furnitures & Show Pieces</h4>
-              <p class="text-gray-600">Gold and silver leafing is widely done on furniture like bed , dining table,
-                center tables, sofas, chairs, TV units etc .Gold Leafing is also done on the center piece and show piece
-                to give attractive look.</p>
-            </div>
-          </div>
-          <div class="flex items-start">
-            <div class="bg-primary p-2 mr-4">
-              <img src="/images/icons/bunglow.svg" alt="bunglow" class="max-w-[52px]" />
-            </div>
-            <div>
-              <h4 class="font-bold text-gray-800 mb-1">Bungalows</h4>
-              <p class="text-gray-600">Leafing is done on ceiling, walls , furniture, pooja room ,Bar.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <Services />
   <div class="showpieces | py-20 relative">
     <div class="curtain "></div>
     <div class="container">
@@ -219,6 +138,7 @@
 <script setup>
 import Button from '~/components/common/LinkButton.vue'
 import TestimonialSlider from '~/components/common/TestimonialSlider.vue'
+import Services from '~/components/pages/Services.vue'
 
 definePageMeta({
   layout: 'homepage'
